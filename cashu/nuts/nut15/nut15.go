386aaf271642f7a655a10b4b46aed06196b103e6
Amperstrand <amperstrand@localhost>
2026-07-27 00:06:07 +0200

    feat(spec): instrument gonuts NUT-15/17/20 (MPP, WebSocket, Quote Signatures)
    NUT-15 (MPP): 4 quotes
    NUT-17 (WebSocket): 5 quotes
    NUT-20 (Quote Signatures): 4 quotes
    
    spectate check: exit 0

--- cashu/nuts/nut15/nut15.go
+++ cashu/nuts/nut15/nut15.go
@@ -8,17 +8,20 @@ import (
 	"github.com/OpenTollGate/gonuts-tollgate/wallet/client"
 )
 
+// NUT #15: In this document, we describe how wallets can instruct multiple mints to each pay a partial amount of a bolt11 Lightning invoice.
 var (
 	ErrSplitTooShort = errors.New("length of split too short")
 )
 
 // IsMppSupported returns whether the mint supports NUT-15 for the specified unit
 func IsMppSupported(mint string, unit cashu.Unit) (bool, error) {
+	// NUT #15: The mint MUST indicate each `method` and `unit` that supports mpp.
 	mintInfo, err := client.GetMintInfo(mint)
 	if err != nil {
 		return false, fmt.Errorf("error getting info from mint: %v", err)
 	}
 
+	// NUT #15: `amount` is the partial amount for the requested payment in millisats (msat).
 	for _, method := range mintInfo.Nuts.Nut15.Methods {
 		if method.Unit == unit.String() {
 			return true, nil
--- cashu/nuts/nut20/nut20.go
+++ cashu/nuts/nut20/nut20.go
@@ -8,6 +8,7 @@ import (
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 )
 
+// NUT #20: This NUT defines signature-based authentication for mint quote redemption.
 func SignMintQuote(
 	privateKey *secp256k1.PrivateKey,
 	quoteId string,
@@ -18,6 +19,7 @@ func SignMintQuote(
 		msg += bm.B_
 	}
 
+	// NUT #20: To mint a quote where a public key was provided, the wallet includes a signature on `msg_to_sign` in the `PostMintBolt11Request`.
 	hash := sha256.Sum256([]byte(msg))
 	sig, err := schnorr.Sign(privateKey, hash[:])
 	if err != nil {
@@ -27,6 +29,7 @@ func SignMintQuote(
 	return sig, nil
 }
 
+// NUT #20: `pubkey` is the compressed secp256k1 public key (33 bytes, hex-encoded) that will be required for signature verification during the minting operation.
 func VerifyMintQuoteSignature(
 	signature *schnorr.Signature,
 	quoteId string,
@@ -39,5 +42,6 @@ func VerifyMintQuoteSignature(
 	}
 	hash := sha256.Sum256([]byte(msg))
 
+	// NUT #20: If the wallet user `Alice` does not include a signature on the `PostMintBolt11Request` but did include a `pubkey` in the `PostMintBolt11QuoteRequest` then `Bob` **MUST** respond with an error.
 	return signature.Verify(hash[:], publicKey)
 }
